8821ab95725aa1aafa71bd105850829c57752342
arthurtsenin <arturcenin@gmail.com>
2023-01-11 12:07:34 +0300

    test commit

---
+++ prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: "avoid",
+  jsxBracketSameLine: false,
+  trailingComma: "all",
+  semi: false,
+};
